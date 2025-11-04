c33e283bc86a6b5657c069826772ffd2fa48e50b
Armita Tavakolshoar <armitatavakol@gmail.com>
2025-11-04 18:33:10 +0300

    tests

--- grades/tests.py
+++ grades/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Grade
+from django.urls import reverse
 
-# Create your tests here.
+class GradeAverageViewTest(TestCase):
+    def setUp(self):
+        Grade.objects.create(score=90)
+        Grade.objects.create(score=80)
+        Grade.objects.create(score=70)
+
+    def test_average_view(self):
+        response = self.client.get(reverse('average'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "80.0")
