009e170792e6ba0100ba58ceda018a69d2535020
beyz100 <beyzayuz@gmail.com>
2025-11-04 18:09:23 +0300

    Grades admin

--- grades/admin.py
+++ grades/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
+from .models import Grade
 
-# Register your models here.
+
+class CourseFilter(admin.SimpleListFilter):
+    """
+    Grade modelini, ait olduğu Ders'e göre filtrelemek için özel filtre.
+    """
+    title = 'Course'
+    parameter_name = 'course'
+
+    def lookups(self, request, model_admin):
+        from courses.models import Course
+        courses = Course.objects.all()
+        return [(c.id, c.code) for c in courses]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(assessment__course__id=self.value())
+
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = (
+        'student_username', 
+        'course_code', 
+        'assessment_type', 
+        'lo_code', 
+        'score_percentage', 
+        'lo_mastery_score'
+    )
+    
+    list_filter = (
+        CourseFilter, 
+        'assessment__type', 
+        'learning_outcome__code',
+    )
+    
+    
+    search_fields = (
+        'student__username', 
+        'student__first_name', 
+        'student__last_name', 
+        'assessment__course__code',
+        'learning_outcome__code',
+    )
+    
+    fieldsets = (
+        (None, {'fields': ('student', 'assessment', 'learning_outcome')}),
+        ('Scores', {'fields': ('score_percentage', 'lo_mastery_score')}),
+    )
+
+    @admin.display(description='Student')
+    def student_username(self, obj):
+        return obj.student.username
+
+    @admin.display(description='Course Code')
+    def course_code(self, obj):
+        return obj.assessment.course.code
+
+    @admin.display(description='Assessment Type')
+    def assessment_type(self, obj):
+        return obj.assessment.get_type_display()
+
+    @admin.display(description='LO Code')
+    def lo_code(self, obj):
+        return obj.learning_outcome.code
