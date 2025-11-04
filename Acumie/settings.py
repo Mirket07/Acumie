30fed9738f76398ce82fd470495483ecac7cf85e
beyz100 <beyzayuz@gmail.com>
2025-11-04 12:09:54 +0300

    Abstract User

--- Acumie/settings.py
+++ Acumie/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'course',
+    'grades',
+    'outcomes',
+    'reports'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class UserRole(models.TextChoices):
+    STUDENT = 'STUDENT', 'Öğrenci'
+    INSTRUCTOR = 'INSTRUCTOR', 'Eğitmen'
+    DEPT_HEAD = 'DEPT_HEAD', 'Departman Başkanı'
+    ADMIN = 'ADMIN', 'Yönetici'
+
+class CustomUser(AbstractUser):
+    role = models.CharField(
+        max_length=20,
+        choices=UserRole.choices,
+        default=UserRole.STUDENT,
+        verbose_name="Kullanıcı Rolü"
+    )
+
+class Meta:
+        verbose_name = "Kullanıcı"
+        verbose_name_plural = "Kullanıcılar"
+
+def __str__(self):
+        display_name = self.email if self.email else self.username
+        return f"{display_name} ({self.get_role_display()})"
+
