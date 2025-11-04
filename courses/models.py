c765a995bfc66805824ed333e85b33584827110f
Mirket07 <cerenyurdunol@gmail.com>
2025-11-04 12:48:47 +0300

    course models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
+from accounts.models import UserRole
 
-# Create your models here.
+
+class Course(models.Model):
+    code=models.CharField(max_length=20)
+    name=models.CharField(max_length=200)
+    credits=models.DecimalField(max_digits=4,decimal_places=2)
+
+class Enrollment(models.Model):
+    student=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,limit_choices_to={'role':UserRole.STUDENT})
+    course=models.ForeignKey(Course,on_delete=models.CASCADE)
