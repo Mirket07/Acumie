4fc335026e6a31265cb32620b23865e63b3bf9f4
beyz100 <beyzayuz@gmail.com>
2025-11-04 13:03:19 +0300

    Department Head için Admin Paneli

--- outcomes/admin.py
+++ outcomes/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import ProgramOutcome, LearningOutcome, LO_PO_Contribution
 
-# Register your models here.
+class LO_PO_ContributionInline(admin.TabularInline):
+    model = LO_PO_Contribution
+    extra = 1 
+    fields = ('program_outcome', 'contribution_percentage',)
+
+
+@admin.register(LearningOutcome)
+class LearningOutcomeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'title')
+    search_fields = ('code', 'title')
+    inlines = [LO_PO_ContributionInline]
+
+
+@admin.register(ProgramOutcome)
+class ProgramOutcomeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'title')
+    search_fields = ('code', 'title')
+
