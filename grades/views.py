6f890e5bcbd46f3c88f5e91b3cf758fa88ca77ed
Armita Tavakolshoar <armitatavakol@gmail.com>
2025-11-04 18:24:11 +0300

    view.py

--- grades/views.py
+++ grades/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.db.models import Avg
+from .models import Grade  
 
-# Create your views here.
+def average_view(request):
+    
+    avg_score = Grade.objects.aggregate(Avg('score'))['score__avg']
+
+    
+    return render(request, 'average.html', {'average': avg_score})
