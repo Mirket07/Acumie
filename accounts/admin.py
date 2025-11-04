4c3fe6781080057af62339c764a3b4816bcc54e4
beyz100 <beyzayuz@gmail.com>
2025-11-04 12:23:11 +0300

    CustomUser modeli eklendi

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'role')
+    list_filter = ('role', 'is_staff', 'is_superuser', 'is_active')
+
+admin.site.register(CustomUser, CustomUserAdmin)
