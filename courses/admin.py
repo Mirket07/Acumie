1ff7d96bb84d075ffed1d1289468bd8d7f0dba95
Armita Tavakolshoar <armitatavakol@gmail.com>
2025-11-05 22:22:41 +0300

    admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Course, Assessment
 
-# Register your models here.
+class AssessmentInline(admin.TabularInline):
+    model = Assessment
+    extra = 1
+    filter_horizontal = ('learning_outcomes',)
+    fields = ('type', 'weight_percentage', 'learning_outcomes',)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('code', 'title', 'ects_credit')
+    search_fields = ('code', 'title')
+    inlines = [AssessmentInline]
+
+@admin.register(Assessment)
+class AssessmentAdmin(admin.ModelAdmin):
+    list_display = ('course', 'type', 'weight_percentage')
+    list_filter = ('course', 'type')
+    filter_horizontal = ('learning_outcomes',)
