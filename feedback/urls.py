30efe3d0a176ec93f1b804205260d1152111cbfc
beyz100 <beyzayuz@gmail.com>
2025-11-07 22:10:00 +0300

    feedback urls

---
+++ feedback/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'feedback'
+
+urlpatterns = [
+    # Geri bildirim formu için yer tutucu
+    # path('submit/', views.feedback_submit_view, name='submit'), 
+]
