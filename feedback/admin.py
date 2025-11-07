3db4342e0ec212cfdcf3a64615634e155683edff
beyz100 <beyzayuz@gmail.com>
2025-11-07 22:08:07 +0300

    feedback admin

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Feedback
 
-# Register your models here.
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('course', 'created_at',)
+    list_filter = ('course', 'created_at')
+    search_fields = ('feedback_text',)
+    readonly_fields = ('created_at',)
