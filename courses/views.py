edd2b7b29514d87b2c583e377869c438a320a07e
beyz100 <beyzayuz@gmail.com>
2025-12-03 19:51:41 +0300

    kursa tıklama

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Course
+from grades.models import Grade
 
-# Create your views here.
+def course_detail_view(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    sections = course.sections.prefetch_related('materials').all()
+    
+    student_grades = []
+    if request.user.is_authenticated and request.user.role == 'STUDENT':
+        student_grades = Grade.objects.filter(
+            student=request.user,
+            assessment__course=course
+        ).select_related("assessment", "learning_outcome")
+
+    context = {
+        'course': course,
+        'sections': sections,
+        'student_grades': student_grades,
+    }
+    return render(request, 'courses/course_detail.html', context)
