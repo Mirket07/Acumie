13d0d16f75b998c43afdee314731116dc357ceef
beyz100 <beyzayuz@gmail.com>
2025-12-07 21:12:19 +0300

    Notlar tablosu ve feedback request eklendi

--- feedback/urls.py
+++ feedback/urls.py
@@ -6,4 +6,5 @@ app_name = 'feedback'
 urlpatterns = [
     path('submit/', views.feedback_submit_view, name='submit'), 
     path('thank-you/', views.feedback_thank_you_view, name='thank_you'),
+    path('request/<int:assessment_id>/', views.request_feedback, name='request_feedback'),
 ]
